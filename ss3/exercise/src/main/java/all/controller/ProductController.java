c8be1e697c4cc392d5844970dc6d642c47fbc467
nguyenhuuduc3888 <nguyenhuuduc3888@gmail.com>
2022-07-24 09:59:03 +0700

    up

--- ss3/exercise/src/main/java/all/controller/ProductController.java
+++ ss3/exercise/src/main/java/all/controller/ProductController.java
@@ -17,84 +17,12 @@ import java.util.Random;
 public class ProductController {
     IProductService productService = new ProductService();
 
-//    @GetMapping("/index")
-//    public String index(Model model) {
-//        List<Product> productList = productService.finAll();
-//        model.addAttribute("products", productList);
-//        return "/index";
-//    }
-//
-//    @GetMapping("/create")
-//    public String showFormCreate(Model model) {
-//        model.addAttribute("product", new Product());
-//        return "/create";
-//    }
-//
-//    @PostMapping("/save")
-//    public String save(Product product, RedirectAttributes redirectAttributes) {
-//        product.setProductId((int) (Math.random() * 1000));
-//        productService.save(product);
-//        redirectAttributes.addFlashAttribute("success", "Them moi ok!");
-//        return "redirect:/index";
-//    }
-//
-//    @GetMapping("/{id}/edit")
-//    public String showFormEdit(@PathVariable int id, Model model) {
-//        Product product = productService.findById(id);
-//        model.addAttribute("product", product);
-//        return "/edit";
-//    }
-//
-//    @PostMapping("/edit")
-//    public String edit(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
-//        productService.update(product.getProductId(), product);
-//        redirectAttributes.addFlashAttribute("success", "Edit ok !");
-//        return "redirect:/index";
-//    }
-//
-//    @GetMapping("/{id}/delete")
-//    public String showFormDelete(@PathVariable int id, Model model) {
-//        Product product = productService.findById(id);
-//        model.addAttribute("product", product);
-//        return "/delete";
-//    }
-//
-//    @PostMapping("/delete")
-//    public String delete(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
-//        productService.delete(product.getProductId());
-//        redirectAttributes.addFlashAttribute("success", "Delete ok !");
-//        return "redirect:/index";
-//    }
-//
-//    @GetMapping("/{id}/view")
-//    public String view(@PathVariable int id, Model model) {
-//        Product product = productService.findById(id);
-//        model.addAttribute("product", product);
-//        return "/view";
-//    }
-//
-//    @GetMapping("/search")
-//    public String searchByName(@RequestParam String name, Model model) {
-//        List<Product> products = productService.findByName(name);
-//        model.addAttribute("products", products);
-//        return "/index";
-//    }
-//Hie
-
-
-
 
     @GetMapping("/index")
     public String showList(Model model) {
         model.addAttribute("products", productService.finAll());
-        return "/index";
-    }
-
-    //Thêm
-    @GetMapping("/create")
-    public String showFormCreate(Model model) {
         model.addAttribute("product", new Product());
-        return "/create";
+        return "/index";
     }
 
     @PostMapping("/save")
@@ -104,33 +32,19 @@ public class ProductController {
         return "redirect:index";
     }
 
-    //Sửa
-    @GetMapping("/{id}/edit")
-    public String showFormEdit(@PathVariable int id, Model model) {
-        Product product = productService.findById(id);
-        model.addAttribute("product", product);
-        return "/edit";
-    }
-
     @PostMapping("/edit")
-    public String edit(@ModelAttribute Product product) {
+    public String edit(
+            @RequestParam int id,
+            @RequestParam String name,
+            @RequestParam double price,
+            @RequestParam String description,
+            @RequestParam String producer
+    ) {
+        Product product = new Product(id, name, price, description, producer);
         productService.update(product.getProductId(), product);
         return "redirect:index";
     }
 
-    //Xóa
-//    @GetMapping("/{id}/delete")
-//    public String showFormDelete(@PathVariable int id, Model model) {
-//        Product product = productService.findById(id);
-//        model.addAttribute("product", product);
-//        return "/delete";
-//    }
-//
-//    @PostMapping("/delete")
-//    public String delete(@ModelAttribute("product") Product product) {
-//        productService.delete(product.getProductId());
-//        return "redirect:index";
-//    }
 
     @GetMapping("/delete")
     public String delete(@RequestParam int id, RedirectAttributes redirect) {
@@ -150,6 +64,7 @@ public class ProductController {
     public String search(@RequestParam String name, Model model) {
         List<Product> products = productService.findByName(name);
         model.addAttribute("products", products);
+        model.addAttribute("product", new Product());
         return "/index";
     }
 }
